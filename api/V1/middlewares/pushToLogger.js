31671cb2587246d402a442e6b86229709d0728b6
Loc IT <loc281202@gmail.com>
2023-08-19 10:45:46 +0700

    feat: write log using dicord.js #7

---
+++ api/V1/middlewares/pushToLogger.js
@@ -0,0 +1,19 @@
+const Logger = require('../../../logs/discord.log.v2');
+
+const pushToLogDiscord = async (req, res, next) => {
+  try {
+    Logger.sendToFormatCode({
+      title: `Method: ${req.method}`,
+      code: req.method === 'GET' ? req.query : req.body,
+      message: `${req.get('host')}${req.originalUrl}`,
+    });
+    return next();
+  } catch (error) {
+    next(error);
+  }
+  return next();
+};
+
+module.exports = {
+  pushToLogDiscord,
+};
